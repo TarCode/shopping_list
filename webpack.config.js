bc2ca0565c843f6f69e84765745df4a927154943
Taariq Isaacs <taariq@uber5.com>
2016-05-03 22:18:52 +0200

    initial commit with folder structure and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './dist'
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    historyApiFallback: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
